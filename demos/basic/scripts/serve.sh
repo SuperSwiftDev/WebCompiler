3af8788762926d1b4cb879896b6ae75101276f51
Colbyn Wadman <hello@colbyn.com>
2025-06-07 17:23:18 -0600

    avoid issues with paths containing spaces

--- demos/basic/scripts/serve.sh
+++ demos/basic/scripts/serve.sh
@@ -2,7 +2,7 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="${SCRIPT_DIR}/../../.."
-cd $PROJECT_DIR/demos/basic
+cd "$PROJECT_DIR/demos/basic"
 
 # http-server -p 8001 output
 
--- scripts/basic-demo/compile.sh
+++ scripts/basic-demo/compile.sh
@@ -2,7 +2,7 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="${SCRIPT_DIR}/../.."
-cd $PROJECT_DIR
+cd "$PROJECT_DIR"
 
 # ./scripts/ssio.sh compile --root sample --template sample/base.html --input "sample/pages/**/*.html" --output output
 
